96e96ed0122adb1c1cf48d395f9d4a9438d62813
Mark Ryan <mark.ryan@gmail.com>
2022-01-14 18:12:19 -0700

    docker file

--- adsb-sl/main.py
+++ adsb-sl/main.py
@@ -1,31 +1,42 @@
 import streamlit as st
 import requests
+import os
 
-#API_URL = "http://docker1:8080/"
-API_URL = "http://localhost:8005/"
+API_URL = os.environ.get("API_URL")
 
 def main():
     st.header("ADSB Dashboard")
     page = st.sidebar.selectbox("Choose a page", ["Homepage", "Models", "Owners"])
 
     if page == "Homepage":
-        st.write("Please select a page on the left.")
+        st.write("Please select a page on the left, or lookup a plane below.")
+        icao = st.text_input("ICAO Lookup", "c0243e")
+        st.write(load_icao(icao))
     elif page == "Models":
         st.title("Aircraft Models")
-        st.table(load_model_data())
+        num = st.slider('Number of rare models', min_value=1, max_value=100, value=15)
+        st.table(load_overall_data(num, "models"))
     elif page == "Owners":
         st.title("Aircraft Owners")
-        st.table(load_owner_data())
+        num = st.slider('Number of rare owners', min_value=1, max_value=100, value=15)
+        st.table(load_overall_data(num, "owners"))
 
 @st.cache
-def load_model_data():
-    response = requests.get(API_URL + "models?end=10")
+def load_icao(icao):
+    response = requests.get(API_URL + "aircraft/" + icao)
     print(response)
     return response.json()
 
 @st.cache
-def load_owner_data():
-    response = requests.get(API_URL + "owners?end=10")
+def load_overall_data(num, type):
+    response = requests.get(API_URL + type + "?end=" + str(num -1))
+    print(response)
+    return response.json()
+
+@st.cache
+def load_daily_data(num, type, day):
+    response = requests.get(API_URL + type + "?end=" + str(num -1))
+    print(response)
     return response.json()
 
 if __name__ == "__main__":
